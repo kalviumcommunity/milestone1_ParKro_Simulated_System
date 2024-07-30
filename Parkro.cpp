2a448df2ebedf02d25f5105fab2e0b6bba4be61e
codingwithustushar <shekhar.tushar1198@gmail.com>
2024-07-31 02:03:05 +0530

    this->pointerused

--- Parkro.cpp
+++ Parkro.cpp
@@ -3,74 +3,73 @@
 #include <string>
 #include <unordered_map>
 using namespace std;
-class Vehicle{
-    private:
+
+class Vehicle {
+private:
     string LicensePlate;
     string ownersDetails;
     string QRcode;
 
-    public:
-    string getLicensePlate(){
-        return LicensePlate; 
+public:
+    string getLicensePlate() {
+        return LicensePlate;
     }
-    void setLicensePlate(string s){
-        LicensePlate = s;
+    void setLicensePlate(string LicensePlate) {
+        this->LicensePlate = LicensePlate;
     }
-    string getownerDetails(){
-        return ownersDetails; 
+    string getownerDetails() {
+        return ownersDetails;
     }
-    void setownerDetails(string x){
-        ownersDetails = x;
+    void setownerDetails(string ownersDetails) {
+        this->ownersDetails = ownersDetails;
     }
-    string getQRcode(){
-        return QRcode; 
+    string getQRcode() {
+        return QRcode;
     }
-    void setQRcode(string y){
-        QRcode = y;
+    void setQRcode(string QRcode) {
+        this->QRcode = QRcode;
     }
 };
-class ParkingSpace{
-    private:
+
+class ParkingSpace {
+private:
     string location;
     bool Availability;
-    
-    public:
-    string getlocation(){
+
+public:
+    string getlocation() {
         return location;
     }
-    void setlocation(string b){
-        location = b;
+    void setlocation(string location) {
+        this->location = location;
     }
-    bool getAvailability(){
+    bool getAvailability() {
         return Availability;
     }
-    void setAvailability(bool a){
-        Availability = a;
+    void setAvailability(bool Availability) {
+        this->Availability = Availability;
     }
-
 };
 
-
-int main(){
+int main() {
     Vehicle obj;
     ParkingSpace obj1;
-    string LicensePlate,ownerDetails,QRCcode,location;
-    cin>>LicensePlate>>ownerDetails>>QRCcode;
+    string LicensePlate, ownerDetails, QRCcode, location;
+
+    cout << "Enter License Plate: ";
+    cin >> LicensePlate;
+    cout << "Enter Owner's Details: ";
+    cin >> ownerDetails;
+    cout << "Enter QR Code: ";
+    cin >> QRCcode;
+
     obj.setLicensePlate(LicensePlate);
     obj.setownerDetails(ownerDetails);
     obj.setQRcode(QRCcode);
-    cout<<obj.getLicensePlate()<<endl;
-    cout<<obj.getownerDetails()<<endl;
-    cout<<obj.getQRcode()<<endl;
 
-
-
-
-    
-
+    cout << "License Plate: " << obj.getLicensePlate() << endl;
+    cout << "Owner's Details: " << obj.getownerDetails() << endl;
+    cout << "QR Code: " << obj.getQRcode() << endl;
 
     return 0;
 }
-
-
-
